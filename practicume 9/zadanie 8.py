57ee7e70cd832aca20577e6dc24daf92c82ce95f
ystepanov-beep <y.stepanov@g.nsu.ru>
2026-03-14 14:22:45 +0700

    Update zadanie 8.py

--- practicume 9/zadanie 8.py
+++ practicume 9/zadanie 8.py
@@ -1,13 +1,13 @@
-with open('input.txt', 'r', encoding='utf-8') as inputfile:
-    lines = inputfile.readlines()
-
-steps = [int(line.strip()) for line in lines]
-
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 months = ['January', 'February', 'March', 'April', 'May', 'June',
           'July', 'August', 'September', 'October', 'November', 'December']
 
-with open('output.txt', 'w', encoding='utf-8') as outputfile:
+with open('input7.txt', 'r', encoding='utf-8') as inputfile:
+    lines = inputfile.readlines()
+
+steps = [int(line.strip()) for line in lines]
+
+with open('output7.txt', 'w', encoding='utf-8') as outputfile:
     start = 0
     for i in range(12):
         days = days_in_month[i]
