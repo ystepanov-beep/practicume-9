0732a38f9387d000956bda17c719b02834a43a9f
ystepanov-beep <y.stepanov@g.nsu.ru>
2026-03-14 14:26:12 +0700

    Update zadanie 8.py

--- practicume 9/zadanie 8.py
+++ practicume 9/zadanie 8.py
@@ -1,5 +1,5 @@
-days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-months = ['January', 'February', 'March', 'April', 'May', 'June',
+DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+MONTHS = ['January', 'February', 'March', 'April', 'May', 'June',
           'July', 'August', 'September', 'October', 'November', 'December']
 
 with open('input7.txt', 'r', encoding='utf-8') as inputfile:
@@ -10,8 +10,8 @@ steps = [int(line.strip()) for line in lines]
 with open('output7.txt', 'w', encoding='utf-8') as outputfile:
     start = 0
     for i in range(12):
-        days = days_in_month[i]
+        days = DAYS_IN_MONTH[i]
         month_steps = steps[start:start + days]
         average = sum(month_steps) / days
-        outputfile.write(months[i] + ': ' + str(average) + '\n')
+        outputfile.write(MONTHS[i] + ': ' + str(average) + '\n')
         start += days
